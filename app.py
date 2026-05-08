397938a130fa1d0b2ddebafb38b238dc958dc8a8
zemmouriouiam-hue <zemmouri.ouiam@gmail.com>
2026-05-08 14:49:38 +0100

    Filter DataFrame by Cross Section if not 'Tous'

--- app.py
+++ app.py
@@ -350,6 +350,7 @@ fdf = df.copy()
 if entity   != "Tous": fdf = fdf[fdf["Entities"] == entity]
 if customer != "Tous": fdf = fdf[fdf["Customer"] == customer]
 if family   != "Tous": fdf = fdf[fdf["Family"] == family]
+if Cross Section   != "Tous": fdf = fdf[fdf["Cross section"] == Cross Section]    
 if len(date_range) == 2:
     fdf = fdf[(fdf["Date"].dt.date >= date_range[0]) &
               (fdf["Date"].dt.date <= date_range[1])]
